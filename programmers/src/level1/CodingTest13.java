64a6b255435238e54f22fccfea02c919e8ad31aa
WookCoding <dnrwhddla@gmail.com>
2024-03-16 19:04:09 +0900

    codingTest

---
+++ programmers/src/level1/CodingTest13.java
@@ -0,0 +1,9 @@
+package level1;
+
+public class CodingTest13 {
+	public static void main(String[] args) {
+		
+	}
+	
+	
+}
--- test/src/test/Testcoding.java
+++ test/src/test/Testcoding.java
@@ -2,16 +2,29 @@ package test;
 
 import java.util.Arrays;
 import java.util.Iterator;
-
+import java.util.LinkedList;
+import java.util.Stack;
+7
 public class Testcoding {
 	public static void main(String[] args) {
-		
+		String s = " kiwi papaya melon ";
+		String answer = "";
+		String[] datas = s.split(" ");
 
-		String a = "0123456789";
-		String t1 = "a234";
-		String t2 = "12347";
-		
-		
-		
+		for(int i =0; i < datas.length; i++) {
+			for(int j = 0; j < datas[i].length(); j++ ) {
+				
+				if(j % 2 != 0) {
+					answer += Character.toLowerCase(datas[i].charAt(j));
+				}else {
+					answer += Character.toUpperCase(datas[i].charAt(j));
+				}
+				
+			}
+			if(datas.length -1 > i && datas[i].length() != 0) {
+				answer += " ";	
+			}
+		}
+		System.out.println(answer);
 	}
 }
